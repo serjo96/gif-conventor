f3d9e17a0ccf8026ece9cc4277638c1d244705e9
serjo96 <promlemboy@gmail.com>
2025-02-21 05:40:20 +0700

    Added new styles

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { UploadModule } from './features/upload/upload.module';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [UploadModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Video to GIF Converter';
+  readonly title = 'Video to GIF Converter';
 }
