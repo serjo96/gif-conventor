f6c4e14841be60fc9af46c9ca55491ac29f56d6e
serjo96 <promlemboy@gmail.com>
2025-02-22 00:49:42 +0700

    Cleanup code;

--- backend/src/services/conversion.service.ts
+++ backend/src/services/conversion.service.ts
@@ -1,7 +1,7 @@
-import { ApiError } from '../types/api.types';
 import { promises as fs } from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 import path from 'path';
+import { ApiError } from '../types/api.types';
 import { conversionQueue } from '../queues';
 
 import { ErrorCode } from '../types/api.types';
